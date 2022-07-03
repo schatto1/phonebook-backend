81dec7ec503735f863b82a78a205f550c3c0d001
Sumon Chattopadhyay <sumon.chattopadhyay@utah.edu>
2022-07-03 15:21:48 -0700

    feat: exercise 3.5 - adds ability to add new phonebook entries
    Adds a POST function to add new resource from body of request
    Generates ID of new resource by using Math.random

--- index.js
+++ index.js
@@ -64,6 +64,38 @@ app.delete('/api/persons/:id', (request, response) => {
   response.status(204).end()
 })
 
+const generateId = () => {
+
+  const max = 1000 //change this to increase possible IDs
+
+  return Math.floor(Math.random() * max)
+}
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+
+  if (!body.name) {
+    return response.status(400).json({
+      error: 'name missing'
+    })
+  }
+  if (!body.number) {
+    return response.status(400).json({
+      error: 'number missing'
+    })
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generateId(),
+  }
+
+  persons = persons.concat(person)
+
+  response.json(person)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
