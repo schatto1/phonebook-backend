713ae859b944d9d5aed61f1fbf9fda5df5e559bc
Sumon Chattopadhyay <sumon.chattopadhyay@utah.edu>
2022-07-03 19:03:38 -0700

    chore: exercise 3.8 - configures morgan to show data sent in HTTP POST
    Creates new morgan token "body" to show request.body
    Returns JSON-stringified request.body in all morgan logs

--- index.js
+++ index.js
@@ -4,7 +4,12 @@ const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
-app.use(morgan('tiny'))
+
+morgan.token('body', (request, response) => {
+  return JSON.stringify(request.body)
+})
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 
 let persons = [
   {
