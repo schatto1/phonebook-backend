a0218786a164121c1800d296bdd57438fe54dd7c
Sumon Chattopadhyay <sumon.chattopadhyay@utah.edu>
2022-07-20 21:37:05 -0700

    feat: exercise 3.13 - adds personSchema and working get api call to index.js
    First step for 3.13. changes the fetching of all data to fetch from database,
    but code still lives in index.js. Next step is to move Mongoose code into own module

--- index.js
+++ index.js
@@ -3,6 +3,28 @@ const express = require('express')
 const morgan = require('morgan')
 const app = express()
 
+const mongoose = require('mongoose')
+
+// DO NOT SAVE YOUR PASSWORD TO GITHUB!!
+const url = `mongodb+srv://fullstack:<password>@cluster0.pmkf0.mongodb.net/phonebookApp?retryWrites=true&w=majority`
+
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+
+personSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+const Person = mongoose.model('Person', personSchema)
+
 app.use(express.json())
 app.use(express.static('build'))
 
@@ -12,31 +34,10 @@ morgan.token('body', (request, response) => {
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 
-let persons = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456"
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523"
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345"
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122"
-  }
-]
-
 app.get('/api/persons', (request, response) => {
-  response.json(persons)
+  Person.find({}).then(persons => {
+    response.json(persons)
+  })
 })
 
 app.get('/info', (request, response) => {
