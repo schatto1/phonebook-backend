684fd946a04c5334658763abed663c9ceb07716c
Sumon Chattopadhyay <sumon.chattopadhyay@utah.edu>
2022-07-24 15:43:06 -0700

    refactor: moves error handling to middleware
    Adds unknownEndpoint and errorHandler methods
    Uses these to handle errors

--- index.js
+++ index.js
@@ -51,10 +51,7 @@ app.delete('/api/persons/:id', (request, response) => {
     .then(result => {
       response.status(204).end()
     })
-    .catch(error => {
-      console.log(error)
-      response.status(500).end()
-    })
+    .catch(error => next(error))
 })
 
 const generateId = () => {
@@ -76,6 +73,24 @@ app.post('/api/persons', (request, response) => {
     response.json(newPerson)
   })
 })
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
